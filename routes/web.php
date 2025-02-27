9ec33fd0bf2200319b3f19918361dc109789c458
jalalk2012 <jalalk2012@gmail.com>
2025-02-27 14:59:26 +0600

    1st commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Auth\Admin\LoginController;
+use App\Http\Controllers\Auth\Admin\RegisterController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->middleware('guest:admin')->group(function () {
+
+    Route::get('register', [RegisterController::class, 'create'])->name('admin.register');
+    Route::post('register', [RegisterController::class, 'store']);
+
+    Route::get('login', [LoginController::class, 'create'])->name('admin.login');
+    Route::post('login', [LoginController::class, 'store']);
+
+});
+
+Route::prefix('admin')->middleware('auth:admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::get('logout', [LoginController::class, 'destroy'])->name('admin.logout');
+
+});
+
